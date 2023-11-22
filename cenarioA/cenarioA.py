dd374af4ac42d7314649798515b68a1b73a0381a
talesNogueira <talescabralnog@gmail.com>
2023-11-22 17:22:23 -0300

    add/fix: cenarios implementation

--- cenarioA/cenarioA.py
+++ cenarioA/cenarioA.py
@@ -1,31 +1,24 @@
 from mininet.net import Mininet
 from mininet.topo import Topo
-from mininet.node import Host, Switch
-from mininet.link import TCLink
 from mininet.cli import CLI
-from mininet.log import setLogLevel
 
-class MyTopology(Topo):
+class nonSDNTopology(Topo):
     def build(self):
         # Adding switches
         switch1 = self.addSwitch('s1')
-        switch2 = self.addSwitch('s2')
 
         # Adding hosts
         host1 = self.addHost('h1')
         host2 = self.addHost('h2')
         host3 = self.addHost('h3')
 
-        # Connecting hosts to switches with a bandwidth limit of 10 Mbps
-        self.addLink(host1, switch1, cls=TCLink, bw=10)
-        self.addLink(host2, switch1, cls=TCLink, bw=10)
-        self.addLink(host3, switch2, cls=TCLink, bw=10)
-
-        # Connecting switches
-        self.addLink(switch1, switch2, cls=TCLink, bw=10)
+        # Connecting hosts to switches
+        self.addLink(host1, switch1)
+        self.addLink(host2, switch1)
+        self.addLink(host3, switch1)
 
 def create_network():
-    topo = MyTopology()
+    topo = nonSDNTopology()
     net = Mininet(topo=topo)
     net.start()
 
@@ -36,5 +29,4 @@ def create_network():
     net.stop()
 
 if __name__ == '__main__':
-    setLogLevel('info')
     create_network()
--- cenarioB/cenarioB.py
+++ cenarioB/cenarioB.py
@@ -1,74 +1,43 @@
 from mininet.net import Mininet
 from mininet.topo import Topo
-from mininet.node import Host, Switch
-from mininet.node import OVSSwitch, Controller, RemoteController
-from mininet.link import TCLink
 from mininet.cli import CLI
-from mininet.log import setLogLevel
 
+from mininet.node import RemoteController
 
-class SDNTopo(Topo):
+class SDNTopology(Topo):
     def build(self):
-        # Add switches
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
+        # Adding switches
+        switch1 = self.addSwitch('s1')
 
-        # Add hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
+        # Adding hosts
+        host1 = self.addHost('h1')
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
 
-        # Add links
-        self.addLink(h1, s1, cls=TCLink, bw=10)
-        self.addLink(h2, s1, cls=TCLink, bw=10)
-        self.addLink(h3, s2, cls=TCLink, bw=10)
+        # Connecting hosts to switches
+        self.addLink(host1, switch1)
+        self.addLink(host2, switch1)
+        self.addLink(host3, switch1)
 
-        # Connecting switches
-        self.addLink(s1, s2, cls=TCLink, bw=10)
-
-def run():
-    # Create Topology
-    topo = SDNTopo()
-
-    # Create Mininet with cleanup
-    # net = Mininet(topo=topo)
-    # Create Mininet with OVSSwitch and RemoteController
-    net = Mininet(topo=topo, switch=OVSSwitch, controller=RemoteController, link=TCLink)
-    #   net.linkDelay('s1', 's2', '10ms', '5ms')
-
-    # Connect each switch to the controller
-    for switch in net.switches:
-        switch.start([])  # Start without a default controller
-
-    # Start controller
-    net.controllers[0].start()
-
-    # Add flow entry to prioritize TCP traffic
-    s1 = net.get('s1')
-    s2 = net.get('s2')
-    #s1.cmd('ovs-ofctl add-flow s1 "priority=65535,ip,nw_proto=6,actions=output:2"')  # Send TCP traffic to port 2 (s2)
-
-
-    # Set OpenFlow rules to prioritize TCP traffic
-    s1.cmd('ovs-ofctl add-flow s1 "priority=65535,ip,nw_proto=6,actions=output:2"')  # Send TCP traffic to port 2 (s2)
-    s2.cmd('ovs-ofctl add-flow s2 "priority=65535,ip,nw_proto=6,actions=output:1"')  # Send TCP traffic to port 1 (s1)
+def create_network():
+    topo = SDNTopology()
+    net = Mininet(topo=topo, controller=RemoteController, autoSetMacs=True)
     
-    # h2 = net.get('h2')
-    # h2.cmd('iperf -s')
-    # h1 = net.get('h1')
-    # h1.cmd('iperf -c 10.0.0.2 -t 10 -b 10M')
-    # h3 = net.get('h3')
-    # h3.cmd('iperf -c 10.0.0.2 -t 10 -b 10M')
-    
-    
-    # Start network
+    # Starting Ryu SDN Controller
+    ryu_controller = net.addController('ryu_controller', controller=RemoteController, ip='127.0.0.1', port=6633)
+        
+    # Start Mininet Network
     net.start()
+    
+    # Connect switch to the Ryu Controller
+    for switch in net.switches:
+        switch.start([ryu_controller])
 
-    # Run the Mininet CLI
+    # Starting Mininet CLI
     CLI(net)
 
-    # Stop Mininet
+    # Stopping the network upon exiting the CLI
     net.stop()
 
 if __name__ == '__main__':
-    run()
+    create_network()
